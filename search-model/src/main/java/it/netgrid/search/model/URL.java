29eed37a2cd7fecf7ee87b536129310861a17955
Alberto Harka <alberto_harka@yahoo.com>
2016-03-31 12:29:09 +0200

    Aggiunta della parte di codice per la persistenaza

--- search-model/src/main/java/it/netgrid/search/model/URL.java
+++ search-model/src/main/java/it/netgrid/search/model/URL.java
@@ -2,20 +2,75 @@ package it.netgrid.search.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.ForeignCollectionField;
+
+
+
+@Entity(name="ulrs")
 public class URL {
-
+	
+	public static final String ID_FIELD_NAME="url_id";
+	public static final String PATH_FIELD_NAME = "url_path";
+	public static final String LANGUAGE_FIELD_NAME = "url_language";
+	public static final String AUTHOR_FIELD_NAME = "url_author";
+	public static final String REGION_FIELD_NAME = "url_region";
+	public static final String DATE_CREATE_FIELD_NAME = "url_date_create";
+	public static final String DATE_VISIT_FIELD_NAME = "url_date_visit";
+	public static final String NUM_CLICK_FIELD_NAME = "url_num_click";
+	public static final String STATE_CODE_FIELD_NAME = "url_state_code";
+	public static final String LAST_CONTENT_FIELD_NAME = "url_last_content";
+	
+	@Id
+	@GeneratedValue
+	@Column(name=ID_FIELD_NAME)
+	private long id;
+	
+	@Column(name=PATH_FIELD_NAME)
 	public String path;
+	
+	@Column(name=LANGUAGE_FIELD_NAME)
 	public String language;
+	
+	@Column(name=AUTHOR_FIELD_NAME)
 	public String author;
+	
+	@Column(name=REGION_FIELD_NAME)
 	public String region;
+	
+	@Column(name=DATE_CREATE_FIELD_NAME)
 	public Date dateCreate;
+	
+	@Column(name=DATE_VISIT_FIELD_NAME)
 	public Date dateVisit;
+	
+	@Column(name=NUM_CLICK_FIELD_NAME)
 	public int nClick;
+	
+	@Column(name=STATE_CODE_FIELD_NAME)
 	public int stateCode;
 	
+	@ForeignCollectionField
+	private ForeignCollection<Content> contents;
+	
+	@OneToOne
+	@JoinColumn(name="LAST_CONTENT_FIELD_NAME", nullable=true)
+	private Content lastContent;
+	
+	
+
+
 	public URL(){}
 	
+
+
 	public URL(String path, String language, String author, String region, Date dateCreate, Date dateVisit, int nClick, int stateCode){
 		this.path=path;
 		this.language=language;
@@ -104,9 +159,32 @@ public class URL {
 
 	
 	public void setStateCode(int stateCode) {
+
 		this.stateCode = stateCode;
 	}
 	
+	public Content getLastContent() {
+		return lastContent;
+	}
+
+	public void setLastContent(Content lastContent) {
+		this.lastContent = lastContent;
+	}
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public ForeignCollection<Content> getContents() {
+		return contents;
+	}
+
+	public void setContents(ForeignCollection<Content> contents) {
+		this.contents = contents;
+	}
 	
 }
