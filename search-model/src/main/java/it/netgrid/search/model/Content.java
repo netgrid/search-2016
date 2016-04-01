e349db7314d26eecccfe6e57d8f0de1753212e19
Michele Grigoli <grigolimichele@gmail.com>
2016-04-01 09:54:03 +0200

    content pronto per pull request

--- search-model/src/main/java/it/netgrid/search/model/Content.java
+++ search-model/src/main/java/it/netgrid/search/model/Content.java
@@ -20,8 +20,7 @@ public class Content {
 	public static final String TIME_RESPONSE_FIELD_NAME="cnt_time_response"; 
 	public static final String LANGUAGE_FIELD_NAME="cnt_language"; 
 	public static final String BODY_RAW_FIELD_NAME="cnt_body_raw"; 
-	public static final String CREATION_DATE_FIELD_NAME="cnt_creation_date"; 
-	
+	public static final String CREATION_DATE_FIELD_NAME="cnt_creation_date"; 	
 	public static final String PARENT_CONTENT_FIELD_NAME="cnt_parent_content";
 	
 	@Id
@@ -41,14 +40,10 @@ public class Content {
 	@Column(name=BODY_RAW_FIELD_NAME)
 	private String bodyRaw; 
 	@Column(name=CREATION_DATE_FIELD_NAME)
-	private Date creationDate;
-	
-	
+	private Date creationDate;		
 	@OneToOne
 	@JoinColumn(name=PARENT_CONTENT_FIELD_NAME)
 	private Document document;
-	
-	
 	
 	public Content(){}
 	
@@ -60,48 +55,60 @@ public class Content {
 		this.language = language;
 		this.bodyRaw = bodyRaw ;
 		this.creationDate = creationDate;
-
 	}
 	
 	public String getStateCode() {
 		return stateCode;
 	}
+	
 	public void setStateCode(String stateCode) {
 		this.stateCode = stateCode;
 	}
+	
 	public String getMime() {
 		return mime;
 	}
+	
 	public void setMime(String mime) {
 		this.mime = mime;
 	}
+	
 	public String getEncoding() {
 		return encoding;
 	}
+	
 	public void setEncoding(String encoding) {
 		this.encoding = encoding;
 	}
+	
 	public BigDecimal getTimeResponse() {
 		return timeResponse;
 	}
+	
 	public void setTimeResponse(BigDecimal timeResponse) {
 		this.timeResponse = timeResponse;
 	}
+	
 	public String getLenguage() {
 		return language;
 	}
+	
 	public void setLenguage(String language) {
 		this.language = language;
 	}
+	
 	public String getBodyRaw() {
 		return bodyRaw;
 	}
+	
 	public void setBodyRaw(String bodyRaw) {
 		this.bodyRaw = bodyRaw;
 	}
+	
 	public Date getCreationDate() {
 		return creationDate;
 	}
+	
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
