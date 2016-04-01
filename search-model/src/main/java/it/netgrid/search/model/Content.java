5afedd1e28f2a48d7d3924304ad002ada79133a2
Michele Grigoli <grigolimichele@gmail.com>
2016-04-01 10:14:05 +0200

    style

--- search-model/src/main/java/it/netgrid/search/model/Content.java
+++ search-model/src/main/java/it/netgrid/search/model/Content.java
@@ -10,44 +10,44 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
-@Entity(name="contents")
+@Entity(name = "contents")
 public class Content { 
-
-	public static final String ID_FIELD_NAME="cnt_id";
-	public static final String STATE_CODE_FIELD_NAME="cnt_state_code"; 
-	public static final String MIME_FIELD_NAME="cnt_mime"; 
-	public static final String ENCODING_FIELD_NAME="cnt_encoding"; 
-	public static final String TIME_RESPONSE_FIELD_NAME="cnt_time_response"; 
-	public static final String LANGUAGE_FIELD_NAME="cnt_language"; 
-	public static final String BODY_RAW_FIELD_NAME="cnt_body_raw"; 
-	public static final String CREATION_DATE_FIELD_NAME="cnt_creation_date"; 	
-	public static final String PARENT_CONTENT_FIELD_NAME="cnt_parent_content";
+	
+	public static final String ID_FIELD_NAME = "cnt_id";
+	public static final String STATE_CODE_FIELD_NAME = "cnt_state_code"; 
+	public static final String MIME_FIELD_NAME = "cnt_mime"; 
+	public static final String ENCODING_FIELD_NAME = "cnt_encoding"; 
+	public static final String TIME_RESPONSE_FIELD_NAME = "cnt_time_response"; 
+	public static final String LANGUAGE_FIELD_NAME = "cnt_language"; 
+	public static final String BODY_RAW_FIELD_NAME = "cnt_body_raw"; 
+	public static final String CREATION_DATE_FIELD_NAME = "cnt_creation_date"; 	
+	public static final String PARENT_CONTENT_FIELD_NAME = "cnt_parent_content";
 	
 	@Id
 	@GeneratedValue	
-	@Column(name=ID_FIELD_NAME)
+	@Column(name = ID_FIELD_NAME)
 	private long id; 
-	@Column(name=STATE_CODE_FIELD_NAME)
+	@Column(name = STATE_CODE_FIELD_NAME)
 	private String stateCode;
-	@Column(name=MIME_FIELD_NAME)
+	@Column(name = MIME_FIELD_NAME)
 	private String mime; 
-	@Column(name=ENCODING_FIELD_NAME)
+	@Column(name = ENCODING_FIELD_NAME)
 	private String encoding;
-	@Column(name=TIME_RESPONSE_FIELD_NAME)
+	@Column(name = TIME_RESPONSE_FIELD_NAME)
 	private BigDecimal timeResponse;
-	@Column(name=LANGUAGE_FIELD_NAME)
+	@Column(name = LANGUAGE_FIELD_NAME)
 	private String language;
-	@Column(name=BODY_RAW_FIELD_NAME)
+	@Column(name = BODY_RAW_FIELD_NAME)
 	private String bodyRaw; 
-	@Column(name=CREATION_DATE_FIELD_NAME)
+	@Column(name = CREATION_DATE_FIELD_NAME)
 	private Date creationDate;		
 	@OneToOne
-	@JoinColumn(name=PARENT_CONTENT_FIELD_NAME)
+	@JoinColumn(name = PARENT_CONTENT_FIELD_NAME)
 	private Document document;
 	
-	public Content(){}
+	public Content() {}
 	
-	public Content(String stateCode, String mime, String encoding, BigDecimal timeResponse, String language, String bodyRaw, Date creationDate){
+	public Content(String stateCode, String mime, String encoding, BigDecimal timeResponse, String language, String bodyRaw, Date creationDate) {
 		this.stateCode = stateCode;
 		this.mime = mime;
 		this.encoding = encoding;
