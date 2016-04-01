a60724af4beacd58c21b7ad3eab99ad628646d2c
Davide Bolla <b2974024@trbvn.com>
2016-04-01 10:15:58 +0200

    re-update

--- search-model/src/main/java/it/netgrid/search/model/Document.java
+++ search-model/src/main/java/it/netgrid/search/model/Document.java
@@ -1,11 +1,14 @@
 package it.netgrid.search.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+
+@Entity(name="Document")
 public class Document {
 	
 	public static final String NPAROLE_FIELD_NAME="nParole";
