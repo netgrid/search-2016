1a47e967fa4edea9297a4c90078aff756c4bb9d3
Roberto Indelicato <indelicatoroberto@hotmail.com>
2016-04-01 10:31:24 +0200

    update

--- search-model/src/main/java/it/netgrid/search/model/ResponseItem.java
+++ search-model/src/main/java/it/netgrid/search/model/ResponseItem.java
@@ -43,6 +43,14 @@ public class ResponseItem {
 		this.data=data;
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getUrl() {
 		return url;
 	}
