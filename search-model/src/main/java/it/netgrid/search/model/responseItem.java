4e60a51b6e5a3a3f13fd531b55fb6f33ad1a748a
Roberto Indelicato <indelicatoroberto@hotmail.com>
2016-04-01 09:39:58 +0200

    ultima modifica

--- search-model/src/main/java/it/netgrid/search/model/responseItem.java
+++ search-model/src/main/java/it/netgrid/search/model/responseItem.java
@@ -1,17 +1,42 @@
 package it.netgrid.search.model;
+
 import java.util.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
 
-public class responseItem {
+@Entity(name="ResponseItem")
+public class ResponseItem {
 	
-	String url,title,bodyPatch;
-	 Date data;
+	response r=new response();
 	
-	public responseItem(){}
+	public static final String ID_FIELD_NAME="Id";
+	public static final String URL_CODE_FIELD_NAME = "Url_code";
+	public static final String TITLE_CODE_FIELD_NAME = "title";
+	public static final String BODY_PATCH_CODE_FIELD_NAME = "body_patch";
+	public static final String DATA_CODE_FIELD_NAME = "data";
 	
-	public responseItem(String url,String title,String bodyPatch,Date data){
+
+	@Id
+	@GeneratedValue
+	@Column(name=ID_FIELD_NAME)	
+	private long id;
+	@Column(name=URL_CODE_FIELD_NAME)
+	private String url;
+	@Column(name=TITLE_CODE_FIELD_NAME)
+	private String title;
+	@Column(name=BODY_PATCH_CODE_FIELD_NAME)
+	private String bodyPatch;
+	@Column(name=DATA_CODE_FIELD_NAME)
+	private  Date data;
+	
+	public ResponseItem(){}
+	
+	public ResponseItem(String url,String title,String bodyPatch,Date data){
 		this.url=url;
 		this.title=title;
 		this.bodyPatch=bodyPatch;
