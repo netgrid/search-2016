4ee7769f3980642a59833b8b0b33a49b3b439526
Alberto Harka <alberto_harka@yahoo.com>
2016-04-07 12:40:34 +0200

    no message

--- frontend-gui/src/main/java/it/netgrid/search/frontend/resources/IndexReasource.java
+++ frontend-gui/src/main/java/it/netgrid/search/frontend/resources/IndexReasource.java
@@ -1,5 +1,7 @@
 package it.netgrid.search.frontend.resources;
 
+import java.util.Date;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,8 +11,11 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import com.j256.ormlite.dao.ForeignCollection;
+
 import io.codearte.jfairy.Fairy;
 import it.netgrid.search.model.Response;
+import it.netgrid.search.model.ResponseItem;
 
 
 @Path("/index")
@@ -22,12 +27,37 @@ public class IndexReasource {
 	@GET
 	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-	public Response getResponse(@Context HttpHeaders headers, @QueryParam("query") String Query){
-		Response result = new Response();
+	public Response getResponse(@Context HttpHeaders headers, @QueryParam("query") String query){
+		ForeignCollection<ResponseItem> risultati = new ForeignCollection<ResponseItem>();
+		ResponseItem item;
+		
+		//variabili per result
 		int count = fairy.baseProducer().randomBetween(10, 15);
 		String content = fairy.textProducer().sentence();
 		String id = fairy.textProducer().randomString(10);
+		double queryti = fairy.baseProducer().randomBetween(1000, 3000);
 		
+		//variabili per responseItem
+		String url;
+		String title;
+		String bodyPatch;
+		Date date;
+		
+		
+		//inizializza l'item e lo aggiunge alla collezione
+		for(int i=0; i<count; i++){
+			item= new ResponseItem();
+			
+			
+			
+			risultati.add(item);
+		}
+		
+		
+		
+		
+		Response result = new Response(id, query, queryti, count);
+		result.setItems(risultati);
 		return result;
 	}
 }
